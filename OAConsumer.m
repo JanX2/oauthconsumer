d3d56a161474b2849d7f3bce1a2469644d83266b
Richard Buckle <richardb@sailmaker.co.uk>
2012-04-20 06:38:46 +0100

    Fixed leaks
    Dunno what the author was thinking with the const qualifiers here (C++ isms maybe?), but at least the memory mgt is now correct.

--- OAConsumer.m
+++ OAConsumer.m
@@ -32,9 +32,11 @@
 #pragma mark init
 
 - (id)initWithKey:(const NSString *)aKey secret:(const NSString *)aSecret {
-	[super init];
-	self.key = [aKey retain];
-	self.secret = [aSecret retain];
+    if((self = [super init])) 
+    {
+        self.key = [[aKey copy] autorelease];
+        self.secret = [[aSecret copy] autorelease];
+    }
 	return self;
 }
 
